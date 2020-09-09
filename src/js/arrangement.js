0a0fdc32a724f332bd965a1412a832b4355d9c46
Amber Thomas <ProQuestionAsker@gmail.com>
2020-09-09 14:33:20 -0700

    offset position working

--- src/js/arrangement.js
+++ src/js/arrangement.js
@@ -2,6 +2,10 @@ import loadData from './load-data'
 
 const $section = d3.selectAll('[data-js="arrangement"]')
 
+const HOLE_OFFSET = 100
+const BREAKPOINT = 900
+
+
 function cleanData(dat){
     const mapped =  dat.map((d) => ({
         ...d,
@@ -18,13 +22,21 @@ function cleanData(dat){
 }
 
 function loadMaps(data){
-    $section.selectAll('.map').data(data)
+    $section.selectAll('.tile').data(data)
         .join(enter => {
-            const $container = enter.append('div').attr('class', 'map')
+            const $container = enter.append('div').attr('class', 'tile')
 
             $container.append('img').attr('src', d => `assets/images/${d.key}.png`)
         })
+
+        // if (window.innerWidth >= BREAKPOINT) {
+        //     $section.selectAll('.tile:nth-child(even)').style('transform', `translate(0, ${HOLE_OFFSET}px)`)
+        //    // $section.selectAll('.tile:nth-child(odd)').style('margin-top', `${-HOLE_OFFSET }px`)
+        // } else {
+        //     $section.selectAll('.tile').style('transform', 'translate(0, 0)')
+        // }
 }
+
 
 function init(){
     loadData('assets/data/arrangement.csv').then(response => {
